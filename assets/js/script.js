1bc04d4d53e769e3609a50d4d045bcf4f2b6e6e2
William Broussard <wbroussard@me.com>
2020-06-07 20:51:04 -0500

    script updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// Get search history and print it to buttons
-searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
-// this step is skipped if there is no search history
-if (searchHistory) {
-    for (var i = 0; i < searchHistory.length; i++) {
-        var newBtn = $("<button>");
-        newBtn.text(searchHistory[i]);
-        newBtn.addClass("btn btn-lg btn-block search-history-btn")
-        $("#search-history").append(newBtn);
-    }
-}
-
-=======
 $(document).ready(function(){
 
     //temporary variables only for development
@@ -70,4 +56,3 @@ $(document).ready(function(){
         });
     }
 })
->>>>>>> a4c53f37859abf1d613194188a342f7efbf9a053
